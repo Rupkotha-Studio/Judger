4aed11328bb0cb304fbfe0d95e0d859972d4d7a5
amirhamza05 <sk.amirhamza@gmail.com>
2020-07-08 00:59:15 +0300

    Add Update Version System

---
+++ update.php
@@ -0,0 +1,23 @@
+<?php 
+	function getJsonData($url){
+		return json_decode(file_get_contents($url),true);
+	}
+
+	$currentData = getJsonData("info.json");
+	$githubData = getJsonData($currentData['github-info-file']);
+
+	if(!isset($githubData['version']))return;
+
+	if($currentData['version'] == $githubData['version']){
+		echo "Current Version and Github Master Version Is Same";
+		return;
+	}
+
+	exec("rm -r *");
+	$fileName = $githubData['name'];
+	shell_exec("git clone ".$githubData['github-url']);
+	exec("chmod -R 777 $fileName");
+	exec("mv $fileName/* .");
+	exec("rm -r $fileName");
+	echo "Successfully Updated New Version ".$githubData['version'];
+?>
