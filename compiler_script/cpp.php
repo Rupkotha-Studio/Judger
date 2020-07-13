5111131a8bfa69f482ed49f8299eb0561066f21b
amirhamza05 <sk.amirhamza@gmail.com>
2020-07-13 22:19:14 +0300

    Update CPP Compiler

--- compiler_script/cpp.php
+++ compiler_script/cpp.php
@@ -1,130 +1,236 @@
 <?php
-	class CPP{
-		
-		public $compilerName = "g++";
-		public $sourceCodeFileName = "main.cpp";
-		public $inputFileName = "input.txt";
-		public $expectedOutputFileName = "expected_output.txt";
-		public $outputFileName = "output.txt";
-		public $errorFileName = "error.txt";
-		public $executableFile = "a.out";
-		
-		public $compileCommand;	
-		public $commandError;
 
-		public $sourceCode;
-		public $out;
+/*
+|--------------------------------------------------------------------------
+| C and CPP File Execute In Linux Terminal
+|--------------------------------------------------------------------------
+|
+| This class compile and run code and return output.
+|
+| Language: C, CPP, CPP11
+|
+ */
 
-		public $executionTotalTime = 0;
+class CPP
+{
 
-		public $compailerError = 0;
-		public $runTimeError = 0;
+    private $compilerName = "g++";
+    private $compilerData = array();
 
-		public $processResultData = array();
+    /**
+     * A list of the all file name that are use for execution code.
+     *
+     * @var array
+     */
+    protected $file = [
+        'sourceCodeFileName'     => "main.cpp",
+        'inputFileName'          => "input.txt",
+        'expectedOutputFileName' => "expected_output.txt",
+        'outputFileName'         => "output.txt",
+        'errorFileName'          => "error.txt",
+        'executableFile'         => "a.out",
+    ];
 
-		function __construct(){
-			
-		}
+    /**
+     * Process Result Array.
+     *
+     * @var array
+     */
+    protected $processResultData = [
+        'time'   => 0,
+        'memory' => 0,
+    ];
 
-		public function setData($data){
-			
-			$this->sourceCode = $data['sourceCode'];
-			
-			$this->setCompiler($data['language']);
-			//g++ -lm main.cpp
-			$this->compileCommand = $this->compilerName." -lm ".$this->sourceCodeFileName;
+    /**
+     * Set code data and compiler
+     *
+     * @param array $compilerData
+     *
+     * @return void
+     */
+    public function setData($compilerData)
+    {
 
-			//g++ -lm main.cpp 2> error.txt
-			$this->commandError=$this->compileCommand." 2>".$this->errorFileName;
+        /*
+        |--------------------------------------------------------------------------
+        | Compiler Data Key List
+        |--------------------------------------------------------------------------
+        |
+        | sourceCode, input, expectedOutput, timeLimit, language
+        |
+         */
 
-			$maximumTime = $data['timeLimit'] + 0.3;
-			$this->out = "timeout ".$maximumTime."s ./a.out";
-		}
+        $this->compilerData = $compilerData;
+        $this->setCompiler();
 
-		public function setCompiler($languageName){
+    }
 
-			if($languageName == "CPP11"){
-				//g++ --std=c++11 c++14.cpp
-				$this->compilerName = "g++ --std=c++11";
-			}
-			if($languageName == "C"){
-				//gcc main.c
-				$this->compilerName = "gcc";
-				$this->sourceCodeFileName = "main.c";
-			}
-		}
+    /**
+     * Define compiler prefix command and source code file name
+     *
+     * @return void
+     */
+    public function setCompiler()
+    {
 
-		public function execute(){
-			$this->prepareFile();
-			$this->setPermissionFile();
-			$this->compileCode();
-			
-			//$debugStartTime = microtime(true);
-			$this->checkCompailerError();
-			//$debugEndTime = microtime(true);
-			//$totalDebugTime = sprintf('%0.3f', $debugEndTime-$debugStartTime);
-			//echo "$totalDebugTime";
-			
-			$this->runCode();
-			$this->makeProcessData();
-			$this->removeAllProcessFile();
+        /*
+        |--------------------------------------------------------------------------
+        | Compiler prefix linux command
+        |--------------------------------------------------------------------------
+        |
+        | Here the prefix command of all compiler.This command use for compiler code in linux environment.
+        |
+        | C     : gcc
+        | C++   : g++
+        | C++11 : g++ --std=c++11
+        |
+         */
 
-			return $this->processResultData;
-		}
+        $languageName = $this->compilerData['language'];
 
-		public function prepareFile(){
-			$this->makeFile($this->sourceCodeFileName,$this->sourceCode);
-			$this->makeFile($this->errorFileName);
-		}
-		
-		public function compileCode(){
-			shell_exec($this->commandError);
-		}
+        if ($languageName == "CPP11") {
+            $this->compilerName = "g++ --std=c++11";
+        }
+        if ($languageName == "C") {
+            $this->compilerName               = "gcc";
+            $this->file['sourceCodeFileName'] = "main.c";
+        }
+    }
 
-		public function checkCompailerError(){
-			$error=file_get_contents($this->errorFileName);
-			$this->processResultData['compilerMessage'] = $error;
-		}
+    /**
+     * Execute code step by step
+     *
+     * @return array
+     */
+    public function execute()
+    {
+        $this->prepareFile();
+        $this->setPermissionFile();
 
-		public function runCode(){
-			if(trim($this->processResultData['compilerMessage'])!="")return;
-			//head means maximum output file size 5000000 if code is infinite write but output not write infinite
-			$out = $this->out." < ".$this->inputFileName." | head -c 5000000 > ".$this->outputFileName;
-			
-			$executionStartTime = microtime(true);
-			shell_exec($out);
-			$executionEndTime = microtime(true);
+        $compileSuccess = $this->compileCode();
+        if ($compileSuccess) {
+            $this->runCode();
+        }
 
-			$this->executionTotalTime = sprintf('%0.3f', $executionEndTime - $executionStartTime);
-		}
+        $this->removeAllProcessFile();
+        return $this->processResultData;
+    }
 
+    /**
+     * Prepare Source Code File and Error File
+     *
+     * @return array
+     */
+    public function prepareFile()
+    {
+        $this->makeFile($this->file['sourceCodeFileName'], $this->compilerData['sourceCode']);
+        $this->makeFile($this->file['errorFileName']);
+    }
 
-		public function makeProcessData(){
-			$this->processResultData['time'] = $this->executionTotalTime;
-			$this->processResultData['memory'] = 0;
-		}
+    /**
+     * Set file read write permission
+     *
+     * @return array
+     */
+    public function setPermissionFile()
+    {
+        exec("chmod -R 777 " . $this->file['sourceCodeFileName']);
+        exec("chmod 777 " . $this->file['errorFileName']);
+        exec("chmod -R 777 " . $this->file['executableFile']);
+    }
 
+    /**
+     * Compiler Code
+     *
+     * @return bool
+     */
+    public function compileCode()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | Compile Command
+        |--------------------------------------------------------------------------
+        |
+        | This is compiler code in linux environment.
+        |
+        | Command Structure: compilerPrefix -lm fileName 2> errorFileName
+        |
+        | C     : gcc -lm fileName.c 2> errorFileName
+        | C++   : g++ -lm fileName.cpp 2> errorFileName
+        | C++11 : g++ --std=c++11 -lm fileName.cpp 2> errorFileName
+        |
+         */
 
-		public function makeFile($fileName,$fileVal=""){
-			$file=fopen($fileName,"w+");
-			fwrite($file,$fileVal);
-			fclose($file);
-		}
+        $compilerName = $this->compilerName;
+        $sourceFile   = $this->file['sourceCodeFileName'];
+        $errorFile    = $this->file['errorFileName'];
 
-		public function setPermissionFile(){
-			exec("chmod -R 777 ".$this->sourceCodeFileName);  
-			exec("chmod 777 ".$this->errorFileName);
-			exec("chmod -R 777 ".$this->executableFile);
-		}
+        $compileCmd = "$compilerName -lm $sourceFile 2> $errorFile";
 
-		public function removeAllProcessFile(){
-			exec("rm ".$this->sourceCodeFileName);
-			exec("rm *.o");
-			exec("rm ".$this->errorFileName);
-			exec("rm ".$this->executableFile);
-		}
+        shell_exec($compileCmd);
 
-	}
+        $this->processResultData['compilerMessage'] = file_get_contents($this->file['errorFileName']);
 
+        return trim($this->processResultData['compilerMessage']) == "";
+    }
 
-?>
+    /**
+     * Run code when compile passed
+     *
+     * @return void
+     */
+    public function runCode()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | Run executableFile
+        |--------------------------------------------------------------------------
+        |
+        | This is executableFile run function. We set timeout time. This timeout
+        | time increase 0.1 second then code time out time. Make command look like.
+        |
+        | Command: timeout 2s ./a.out < inputFileName | head -c 5000000 > outputFileName
+        |
+        | [head means maximum output file size 5000000 if code is infinite write but
+        | output not write infinite]
+        |
+         */
+
+        $timeOut = $this->compilerData['timeLimit'] + 0.1;
+        $runCmd  = "timeout " . $timeOut . "s ./a.out";
+        $runCmd .= " < " . $this->file['inputFileName'] . " | head -c 5000000 > " . $this->file['outputFileName'];
+
+        $executionStartTime = microtime(true);
+        shell_exec($runCmd);
+        $executionEndTime = microtime(true);
+
+        $executionTotalTime              = sprintf('%0.3f', $executionEndTime - $executionStartTime);
+        $this->processResultData['time'] = $executionTotalTime;
+    }
+
+    /**
+     * Make File
+     *
+     * @return void
+     */
+    public function makeFile($fileName, $fileVal = "")
+    {
+        $file = fopen($fileName, "w+");
+        fwrite($file, $fileVal);
+        fclose($file);
+    }
+
+    /**
+     * Remove all process file
+     *
+     * @return void
+     */
+    public function removeAllProcessFile()
+    {
+        exec("rm " . $this->file['sourceCodeFileName']);
+        exec("rm *.o");
+        exec("rm " . $this->file['errorFileName']);
+        exec("rm " . $this->file['executableFile']);
+    }
+
+}
