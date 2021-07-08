9edf8f2b09675428f5f054186865961234cad4af
amirhamza05 <sk.amirhamza@gmail.com>
2021-07-08 06:16:28 +0600

    Fix Garbase Value Problem

--- src/Helper/Response.php
+++ src/Helper/Response.php
@@ -26,9 +26,9 @@ class Response
             'memory' => "int",
         ];
 
-        //$this->checker_log  = $this->clearGarbaseValue($this->checker_log);
-        //$this->compiler_log = $this->clearGarbaseValue($this->compiler_log);
-        //$this->output       = $this->clearGarbaseValue($this->output);
+        $this->checker_log  = $this->clearGarbaseValue($this->checker_log);
+        $this->compiler_log = $this->clearGarbaseValue($this->compiler_log);
+        $this->output       = $this->clearGarbaseValue($this->output);
 
         if (isset(request()->program_file)) {
             if (trim($this->compiler_log) != "") {
@@ -52,7 +52,27 @@ class Response
 
     public function clearGarbaseValue($text)
     {
-        return preg_replace('/[^(\x20-\x7F)]*/', '', $text);
+        $utf8 = array(
+            '/[áàâãªä]/u' => 'a',
+            '/[ÁÀÂÃÄ]/u'  => 'A',
+            '/[ÍÌÎÏ]/u'   => 'I',
+            '/[íìîï]/u'   => 'i',
+            '/[éèêë]/u'   => 'e',
+            '/[ÉÈÊË]/u'   => 'E',
+            '/[óòôõºö]/u' => 'o',
+            '/[ÓÒÔÕÖ]/u'  => 'O',
+            '/[úùûü]/u'   => 'u',
+            '/[ÚÙÛÜ]/u'   => 'U',
+            '/ç/'         => 'c',
+            '/Ç/'         => 'C',
+            '/ñ/'         => 'n',
+            '/Ñ/'         => 'N',
+            '/–/'         => '-', // UTF-8 hyphen to "normal" hyphen
+            '/[’‘‹›‚]/u'  => ' ', // Literally a single quote
+            '/[“”«»„]/u'  => ' ', // Double quote
+            '/ /'         => ' ', // nonbreaking space (equiv. to 0x160)
+        );
+        return preg_replace(array_keys($utf8), array_values($utf8), $text);
     }
 
     public function json()
