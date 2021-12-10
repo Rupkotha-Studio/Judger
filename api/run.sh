1ba1809580cee1b2aab1f6438c74011449538d51
amirhamza05 <sk.amirhamza@gmail.com>
2021-12-10 13:39:14 +0600

    Fix Resource temporarily unavailable and added control group in isolate sandbox

--- api/run.sh
+++ api/run.sh
@@ -4,8 +4,8 @@ EXTRA_TIME=$2
 WALL_TIME=$3
 RUN_FILE=$4" "$5
 
-isolate --cleanup
-workdir="$(isolate --init)"
+isolate --cg --cleanup
+workdir="$(isolate --cg --init)"
 boxdir=$workdir/box
 cd box
 cp -r . $boxdir
@@ -15,6 +15,6 @@ rFile=.$cDir"/a.out"
 
 chmod -w $boxdir
 
-isolate -p60 -d tmp -M meta  -t $TIME_LIMIT -x $EXTRA_TIME -w $WALL_TIME -k 64000  -f 40000  --run  -- $RUN_FILE < input 2> error > output
+isolate --cg  -p32 --cg-timing --cg-mem=512000 -M meta  -t $TIME_LIMIT -x $EXTRA_TIME -w $WALL_TIME -k 64000  -f 40000  --run  -- $RUN_FILE < input 2> error > output
 
-isolate --cleanup
+isolate --cg --cleanup
