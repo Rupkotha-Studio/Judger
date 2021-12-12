32141909444b84278ef7e3deb4d4a2119bf59e17
amirhamza05 <sk.amirhamza@gmail.com>
2021-12-12 19:02:01 +0600

    Check cpu process limit busy or not

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -8,9 +8,19 @@ class Api
     ];
     public function __construct()
     {
+        $this->checkCpuLimit();
         $this->_call();
         ff()->removeBusy();
     }
+
+    public function checkCpuLimit(){
+        $busyFilesCount = count(glob("*/".ff()->busy));
+        $cpuCount = shell_exec("cat /proc/cpuinfo | grep processor | wc -l");
+        if ($busyFilesCount >= $cpuCount) {
+            new ErrorEx(['All cpu is busy'], 409);
+        }
+    }
+    
     public function _call()
     {
        // new Validation();
